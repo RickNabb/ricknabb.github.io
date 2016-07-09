01149329c0bcbb55a75c18ab0feeaf0bf85de873
RickNabb <nrabb@outlook.com>
2016-07-09 14:37:10 -0400

    Added admin page, admin css classes, and js for admin state switching

---
+++ js/admin.js
@@ -0,0 +1,52 @@
+/**
+* admin.js
+* @author Nick Rabb <nrabb@outlook.com>
+* This file holds all of the administrator functions that this website needs
+*/
+
+// VARIABLES
+// ================
+
+// State enums
+var STATE_NONE = 0;
+var STATE_BLOG_TOOLS = 1;
+
+// FUNCTIONS
+// ================
+
+function authorize() {
+  
+}
+
+function init() {
+  changeState(STATE_NONE);
+}
+
+function showSideBar() {
+  $("#side-toolbar").addClass("active");
+}
+
+function hideSideBar() {
+  $("#side-toolbar").removeClass("active");
+}
+
+function changeState(state) {
+  $("div[id*='state']").fadeOut();
+  $("div[id*='side-toolbar-]").removeClass("active");
+  switch (state) {
+    case (STATE_NONE):
+      $("#state-none").fadeIn();
+      break;
+    case (STATE_BLOG_TOOLS):
+      $("#state-blog-tools").fadeIn();
+      $("#side-toolbar-blog").addClass("active");
+      break;
+  }
+  if (state !== STATE_NONE) {
+    showSideBar();
+  } else {
+    hideSideBar();
+  }
+}
+
+//init();
